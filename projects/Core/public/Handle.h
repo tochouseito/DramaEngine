443c602a4de0742e31ba4f5aa2e745a88bd80f11
tochouseito <146930194+tochouseito@users.noreply.github.com>
2026-01-05 17:22:36 +0900

    Handleの追加

--- projects/Core/public/Handle.h
+++ projects/Core/public/Handle.h
@@ -3,5 +3,30 @@
 
 namespace Drama::Core
 {
+    enum class HandleType : uint8_t
+    {
+        Unknown = 0,
+        Texture = 1,
+        Buffer = 2,
+        Shader = 3,
+        PipelineState = 4,
+        RenderTargetView = 5,
+        DepthStencilView = 6,
+    };
 
+    struct Handle final
+    {
+        uint32_t id = UINT32_MAX;// 無効なハンドルID
+
+        void clear() noexcept
+        {
+            id = UINT32_MAX;
+        }
+
+        explicit operator bool() const noexcept
+        {
+            // 1) 成功判定
+            return id != UINT32_MAX;
+        }
+    };
 }
